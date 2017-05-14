ebf6efe0b4f80d27cf6ddb6c7ec6d800aa6e302f
duyguserbest <serbestduygu@gmail.com>
2017-05-14 16:00:24 +0300

    Initial commit.

---
+++ frp-api/src/main/java/io/duygu/frp/controller/SpellController.java
@@ -0,0 +1,10 @@
+package io.duygu.frp.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by duygu on 14.05.2017.
+ */
+@RestController
+public class SpellController {
+}
---
+++ frp-api/src/main/java/io/duygu/frp/dto/SpellDto.java
@@ -0,0 +1,134 @@
+package io.duygu.frp.dto;
+
+/**
+ * Created by duygu on 14.05.2017.
+ */
+public class SpellDto {
+
+    private String name;
+    private String desc;
+    private String page;
+    private String range;
+    private String components;
+    private String material;
+    private String ritual;
+    private String duration;
+    private String concentration;
+    private String castingTime;
+    private String level;
+    private String school;
+    private String _class;
+    private String higherLevel;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getPage() {
+        return page;
+    }
+
+    public void setPage(String page) {
+        this.page = page;
+    }
+
+    public String getRange() {
+        return range;
+    }
+
+    public void setRange(String range) {
+        this.range = range;
+    }
+
+    public String getComponents() {
+        return components;
+    }
+
+    public void setComponents(String components) {
+        this.components = components;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public String getRitual() {
+        return ritual;
+    }
+
+    public void setRitual(String ritual) {
+        this.ritual = ritual;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getConcentration() {
+        return concentration;
+    }
+
+    public void setConcentration(String concentration) {
+        this.concentration = concentration;
+    }
+
+    public String getCastingTime() {
+        return castingTime;
+    }
+
+    public void setCastingTime(String castingTime) {
+        this.castingTime = castingTime;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String get_class() {
+        return _class;
+    }
+
+    public void set_class(String _class) {
+        this._class = _class;
+    }
+
+    public String getHigherLevel() {
+        return higherLevel;
+    }
+
+    public void setHigherLevel(String higherLevel) {
+        this.higherLevel = higherLevel;
+    }
+}
